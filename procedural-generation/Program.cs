481f9fb63219b3d4b2109e2f5b822000d2c9c183
arthur56betw1n <arthur56betw1n@gmail.com>
2020-07-10 23:36:50 +0500

    Добавьте файлы проекта.

---
+++ procedural-generation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace procedural_generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Map();
+            var bitmap = map.Draw();
+            var path = @"map.bmp";
+            bitmap.Save(path);
+            Process.Start("cmd", @"/c " + path);
+        }
+    }
+}
